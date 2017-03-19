421cd2f50952dde7d2d1556b2ee396c6aac3104b
Catherine Wang <yijin.wang@mail.utoronto.ca>
2017-03-18 21:03:11 -0400

    Part 2 done?

--- naivebayes.py
+++ naivebayes.py
@@ -8,17 +8,20 @@ from shutil import copy2
 import math
 
 P_pos = 0
+P_neg = 0
 
 train_pos = {}
 train_neg = {}
 train_total = {}
 
 count_train_pos = 0
+count_train_neg = 0
 count_train_total = 0
 
 def create_dir(dir_name):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
+
 
 def split_dataset():
     # should put equal amount of reviews into each set 
@@ -50,6 +53,7 @@ def split_dataset():
         copy2(pos_path + '/' + pos[idx_pos[i]], 'validation/pos')
         copy2(neg_path + '/' + neg[idx_neg[i]], 'validation/neg')
 
+
 def get_wordcount(path):
     pos = {}
     neg = {}
@@ -59,8 +63,6 @@ def get_wordcount(path):
     # count 'each document the word is in' -> if a word appear in a doc twice, count is still 1
     # increment count by 1/len(doc) to normalize
     # remove all punctuation
-    #re.split('\W+', string.lower())
-    # list(OrderedDict.fromkeys(l))
     
     for file in os.listdir(path + '/pos'):
         with open(path + '/pos' + '/' + file) as f:
@@ -88,94 +90,106 @@ def get_wordcount(path):
             neg_file_count += 1/length
     return pos, neg, pos_file_count, neg_file_count
 
-def P_pos_given_words(word_list, m, k):
-    global P_pos
-    P_a_all = get_P_a_all(word_list)
-    #print(P_a_all)
-    #print(P_words_given_pos(word_list, m, k))
-    return math.exp(P_words_given_pos(word_list, m, k) - P_a_all) * P_pos
+
+def log_P_class_given_words(word_list, m, k, cla):
+    global P_pos, P_neg
+    P_words_cla = log_sum_P_words_given_class(word_list, m, k, cla)
+    if cla == 1:
+        P_class = P_pos
+    else:
+        P_class = P_neg
+    return P_words_cla * P_class
     
-def P_words_given_pos(word_list, m, k):
-    global train_pos, count_train_pos, train_total
+    
+def log_sum_P_words_given_class(word_list, m, k, cla):
+    global train_pos, train_neg, count_train_pos, count_train_neg
     log_sum = 0
-    for word in train_total:
-        if word in word_list:
+    for word in word_list:
+        # class is positive
+        if cla == 1:
             if word in train_pos:
-                word_count_pos = train_pos[word]
+                word_count = train_pos[word]
             else:
-                word_count_pos = 0
-            p_word = math.log(word_count_pos + m * k)/(count_train_pos + k)
+                word_count = 0
+            p_word = math.log((word_count + m * k)/(count_train_pos + k))
+        # class is negative
         else:
-            if word in train_pos:
-                word_count_pos = train_pos[word]
+            if word in train_neg:
+                word_count = train_neg[word]
             else:
-                word_count_pos = 0
-            p_word = math.log(1 - (word_count_pos + m * k)/(count_train_pos + k))
+                word_count = 0
+            p_word = math.log((word_count + m * k)/(count_train_neg + k))
         log_sum += p_word
 
-    return log_sum
+    return math.exp(log_sum)
+
 
 def predict_review(word_list, m, k):
-    P_pos_review = P_pos_given_words(word_list, m, k)
-    if P_pos_review >= 0.5:
+    P_pos_review = log_P_class_given_words(word_list, m, k, 1)
+    P_neg_review = log_P_class_given_words(word_list, m, k, 0)
+    if P_pos_review >= P_neg_review:
         return 1
     else:
         return 0
         
+        
 def get_performance(path, m, k):
-    correct_count = 0
-    total = len(os.listdir(path + '/pos')) + len(os.listdir(path + '/neg'))
+    pos_correct_count = 0
+    neg_correct_count = 0
+    pos_total = len(os.listdir(path + '/pos'))
+    neg_total = len(os.listdir(path + '/neg'))
     for file in os.listdir(path + '/pos'):
         f = open(path + '/pos' + '/' + file)
         word_list = re.split('\W+', f.read().lower())
         word_list.remove('')
         word_list = list(OrderedDict.fromkeys(word_list))
-        if (predict_review(word_list, m, k) == 1):
-            correct_count += 1
+        predict = predict_review(word_list, m, k)
+        if (predict == 1):
+            pos_correct_count += 1
+    print(path+" positive performance: "+str(pos_correct_count*100/pos_total)+"%")
     for file in os.listdir(path + '/neg'):
         f = open(path + '/neg' + '/' + file)
         word_list = re.split('\W+', f.read().lower())
         word_list.remove('')
         word_list = list(OrderedDict.fromkeys(word_list))
-        if (predict_review(word_list, m, k) == 0):
-            correct_count += 1
-    return (correct_count * 100 / total)
+        predict = predict_review(word_list, m, k)
+        if (predict == 0):
+            neg_correct_count += 1
+    print(path+" negative performance: "+str(neg_correct_count*100/neg_total)+"%")
+    return ((pos_correct_count+neg_correct_count) * 100 / (pos_total+neg_total))
 
-def get_P_a_all(word_list):
-    global train_total, count_train_total
-    log_sum = 0.0
-    for word in train_total:
-        if word in word_list:
-            p_word = train_total[word]/count_train_total
-        else:
-            p_word = 1 - train_total[word]/count_train_total
-        log_sum += math.log(p_word)
-    #print(log_sum)
-    return log_sum
 
 def main():
     
-    global train_pos, train_neg, train_total, count_train_pos, P_pos, count_train_total
+    global train_pos, train_neg, train_total
+    global count_train_pos, count_train_neg, count_train_total
+    global P_pos, P_neg
     
     #split_dataset()
     train_pos, train_neg, count_train_pos, count_train_neg = get_wordcount('train')
-
+    
     train_total = {k: train_pos.get(k, 0) + train_neg.get(k, 0) for k in set(train_pos) | set(train_neg)}
     
-    #print(train_total)
-    
-    m = 0.1 # m should be float
-    k =  10 # k should be integer
+    m = 0.2
+    k = 300
     
     count_train_total = count_train_pos + count_train_neg
     
     P_pos = count_train_pos / count_train_total
+    P_neg = count_train_neg / count_train_total
 
     train_per = get_performance('train', m, k)
     val_per = get_performance('validation', m, k)
     test_per = get_performance('test', m, k)
     
-    #print ("Training performance: "+str(train_per)+"%")
+    # for m in np.arange(0.1, 0.3, 0.01):
+    #     for k in np.arange(270, 300, 1):
+    #         val_per = get_performance('validation', m, k)
+    #         if (val_per > 70):
+    #             print ("k="+str(k)+", m="+str(m)+", performance: "+str(val_per)+"%")
+    
+    
+    print ("Training performance: "+str(train_per)+"%")
     print ("Validation performance: "+str(val_per)+"%")
     print ("Test performance: "+str(test_per)+"%")
 
